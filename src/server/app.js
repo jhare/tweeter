599d887b246e2cb3729148b5fb2724fdd882dd29
unknown <Administrator@Chad-PC.reaganresources.local>
2015-10-07 17:35:20 -0500

    Fix mixup

--- src/server/app.js
+++ src/server/app.js
@@ -3,12 +3,6 @@ var app = express();
 
 app.use(express.static('./dist'));
 
-app.get('/', function what(req, res) {
-  res.send({
-    'this is': 'some JSON'
-  });
-});
-
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
